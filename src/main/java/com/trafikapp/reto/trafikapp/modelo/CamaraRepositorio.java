8dbc8b0f01ff01bfd083935ab18886ee8c7c79b2
MarkosMartinez <ikbdk@plaiaundi.net>
2025-01-20 13:26:55 +0100

    Conversion de las cordenadas invalidas

--- src/main/java/com/trafikapp/reto/trafikapp/modelo/CamaraRepositorio.java
+++ src/main/java/com/trafikapp/reto/trafikapp/modelo/CamaraRepositorio.java
@@ -6,13 +6,50 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
+import org.locationtech.proj4j.ProjCoordinate;
+import org.locationtech.proj4j.CRSFactory;
+import org.locationtech.proj4j.CoordinateTransform;
+import org.locationtech.proj4j.CoordinateTransformFactory;
+
 
 public interface CamaraRepositorio extends JpaRepository<Camara, Integer> {
+    
 
     public default List<Camara> findAllFiltered() {
+
+        // Transformación de coordenadas
+        CRSFactory crsFactory = new CRSFactory();
+        CoordinateTransformFactory transformFactory = new CoordinateTransformFactory();
+        var utmCRS = crsFactory.createFromName("EPSG:25830");
+        var wgs84CRS = crsFactory.createFromName("EPSG:4326");
+        CoordinateTransform transform = transformFactory.createTransform(utmCRS, wgs84CRS);
+    
+        // Filtrado de cámaras
         return this.findAll().stream()
-            .filter(camara -> camara.getLatitude() != null && camara.getLongitude() != null && camara.getUrlImage() != null && (camara.getUrlImage().startsWith("http://www.bizkaimove.com") || camara.getUrlImage().startsWith("https://www.vitoria-gasteiz.org"))).toList(); //camara.getAddress() != null && camara.getKilometer() != null
+            .filter(camara -> camara.getUrlImage() != null 
+                              && (camara.getUrlImage().startsWith("http://www.bizkaimove.com") 
+                              || camara.getUrlImage().startsWith("https://www.vitoria-gasteiz.org")))
+            // Transformación de coordenadas de UTM a Cordenadas Geográficas
+            .map(camara -> {
+                Double latitude = camara.getLatitude();
+                Double longitude = camara.getLongitude();
+    
+                if (latitude != null && longitude != null) {
+                    if (latitude > 90) {
+                        ProjCoordinate utmCoord = new ProjCoordinate(longitude, latitude);
+                        ProjCoordinate geoCoord = new ProjCoordinate();
+    
+                        transform.transform(utmCoord, geoCoord);
+    
+                        camara.setLatitude(geoCoord.y);
+                        camara.setLongitude(geoCoord.x);
+                    }
+                }
+                return camara;
+            })
+            .toList();
     }
+
     List<Camara> findBySourceId(Integer sourceId);
 
     @Modifying
